fe712d3a6b4017b0ceade17c173687b396abad19
Aditya Dasamantharao <adityadasamantharao@Adityas-MacBook-Air.local>
2026-06-03 22:58:30 +0530

    feat: register Capgo SocialLogin plugin intent handling in android MainActivity

--- android/app/src/main/java/com/watchhive/app/MainActivity.java
+++ android/app/src/main/java/com/watchhive/app/MainActivity.java
@@ -1,5 +1,24 @@
 package com.watchhive.app;
 
+import android.content.Intent;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.PluginHandle;
+import ee.forgr.capacitor.social.login.GoogleProvider;
+import ee.forgr.capacitor.social.login.SocialLoginPlugin;
+import ee.forgr.capacitor.social.login.ModifiedMainActivityForSocialLoginPlugin;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity implements ModifiedMainActivityForSocialLoginPlugin {
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode >= GoogleProvider.REQUEST_AUTHORIZE_GOOGLE_MIN && 
+            requestCode < GoogleProvider.REQUEST_AUTHORIZE_GOOGLE_MAX) {
+            
+            PluginHandle pluginHandle = getBridge().getPlugin("SocialLogin");
+            if (pluginHandle != null) {
+                ((SocialLoginPlugin) pluginHandle.getInstance()).handleGoogleLoginIntent(requestCode, data);
+            }
+        }
+    }
+}
